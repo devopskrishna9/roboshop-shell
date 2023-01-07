57d5bedee4707a3e382a9da49fd05f49cf7bcbc3
krishna <brkkrishna9@gmail.com>
2023-01-07 18:05:40 +0530

    added commands

--- frontend.sh
+++ frontend.sh
@@ -1,3 +1,14 @@
 #front end web content
 echo  this is sample front end web content from nginx webserver
 
+yum install nginx -y
+systemctl enable nginx
+systemctl start nginx
+rm -rf /usr/share/nginx/html/*
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+
+cp files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+
+systemctl restart nginx
