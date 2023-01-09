27c1e27f66de1937f3d32ade1678831b41d9dd43
krishna <brkkrishna9@gmail.com>
2023-01-09 16:22:59 +0530

    new scripts

--- mongodb.sh
+++ mongodb.sh
@@ -1,10 +1,16 @@
-script_location=$(pwd)
-cp ${script_location}/files/mongo.repo /etc/yum.repos.d/mongo.repo
+source common.sh
 
-yum install mongodb-org -y
+print_head "Copy files"
+cp ${script_location}/files/mongo.repo /etc/yum.repos.d/mongo.repo &>>${LOG}
+
+print_head "Install mongodb"
+yum install mongodb-org -y &>>${LOG}
 
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
-systemctl enable mongod
-systemctl start mongod
+print_head "Enable mongodb"
+systemctl enable mongod &>>${LOG}
+
+print_head "start mongodb"
+systemctl start mongod &>>${LOG}
 
