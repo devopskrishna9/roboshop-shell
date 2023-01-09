08b7112d809e982b42d0f0e81c35e1a1de5d0749
krishna <brkkrishna9@gmail.com>
2023-01-09 16:25:31 +0530

    new scripts

--- mongodb.sh
+++ mongodb.sh
@@ -2,15 +2,18 @@ source common.sh
 
 print_head "Copy files"
 cp ${script_location}/files/mongo.repo /etc/yum.repos.d/mongo.repo &>>${LOG}
+status_check
 
 print_head "Install mongodb"
 yum install mongodb-org -y &>>${LOG}
+status_check
 
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 print_head "Enable mongodb"
 systemctl enable mongod &>>${LOG}
+status_check
 
 print_head "start mongodb"
 systemctl start mongod &>>${LOG}
-
+status_check
