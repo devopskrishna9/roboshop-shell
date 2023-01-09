f35b97478ab2c5aad1914d2121d95fed9679001f
krishna <brkkrishna9@gmail.com>
2023-01-09 17:15:21 +0530

    new scripts

---
+++ redis.sh
@@ -0,0 +1,29 @@
+source common.sh
+
+print_head "Install redis repo file"
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>${LOG}
+status_check
+
+print_head "dnf module enable new version"
+dnf module enable redis:remi-6.2 -y &>>${LOG}
+status_check
+
+print_head "Install redis"
+yum install redis -y &>>${LOG}
+status_check
+
+print_head "Binding ports "
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf & /etc/redis/redis.conf &>>${LOG}
+status_check
+
+print_head "Enable the redis"
+systemctl enable redis &>>${LOG}
+status_check
+
+print_head "Start the redis"
+systemctl start redis &>>${LOG}
+status_check
+
+print_head "Restart the redis"
+systemctl restart redis &>>${LOG}
+status_check
