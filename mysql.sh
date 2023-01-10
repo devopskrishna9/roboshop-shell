f311337238fb02ae222a5e06db6ed9d8550bd8eb
krishna <brkkrishna9@gmail.com>
2023-01-10 19:14:19 +0530

    new scripts

--- mysql.sh
+++ mysql.sh
@@ -2,9 +2,8 @@ source common.sh
 
 if [ -z "${root_mysql_password}" ]; then
   echo "Variable root_mysql_password is missing"
-  exit 1
+  exit
 fi
-
 
 print_head "Disable MySQL Default Module"
 dnf module disable mysql -y &>>${LOG}
@@ -23,9 +22,17 @@ systemctl enable mysqld &>>${LOG}
 status_check
 
 print_head "Start MySQL"
-systemctl restart mysqld &>>${LOG}
+systemctl start mysqld &>>${LOG}
 status_check
 
 print_head "Reset Default Database Password"
-mysql_secure_installation --set-root-pass "${root_mysql_password}" &>>${LOG}
+mysql_secure_installation --set-root-pass ${root_mysql_password} &>>${LOG}
+status_check
+
+print_head "check the new password"
+mysql -uroot -p${root_mysql_password} &>>${LOG}
+status_check
+
+print_head "Start MySQL"
+systemctl restart mysqld &>>${LOG}
 status_check
