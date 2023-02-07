945d80732165e1f47aa2da7093ae0cd02b35d5f7
krishna <brkkrishna9@gmail.com>
2023-02-07 18:13:57 +0530

    new changes

--- full-setup.sh
+++
@@ -1,6 +0,0 @@
-for component in frontend cart catalogue user shipping payment redis mongodb mysql rabbitmq ; do
-
- ansible-playbook -i ${component}-dev.devopsnew9.online, roboshop.yml -e HOSTS=all -e ROLE_NAME=${component} -e ansible_user=centos -e ansible_password=DevOps321 -e env=dev
-
-done
-
